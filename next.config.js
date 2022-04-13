13763f79338283375eca6799e401fae405441a2f
valyukhov <alexzed@bk.ru>
2022-04-13 16:48:13 +0300

    add setup for eslint and prettier

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    dirs: ['pages', 'utils', 'components'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
 }
 
 module.exports = nextConfig
