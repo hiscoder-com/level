e3286f777959c8f7f3c94686e36e761a0428f5d5
Pavel <braurs64@gmail.com>
2022-05-06 12:16:46 +0400

    fix position for RU-EN tag

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,30 @@
 import Head from 'next/head'
-import Image from 'next/image'
+
 export default function Home() {
   return (
-    <div className="container pt-24 px-40 font-sans">
+    <div className="container mx-auto relative px-3 font-sans min-h-screen flex flex-col justify-center">
       <Head>
         <title>V-CANA</title>
         <meta name="description" content="VCANA" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="">
-        <div className="flex space-x-3 text-sm font-bold pb-40 justify-end">
-          <div>
-            <a className="text-teal-500 " href="#">
-              RU
-            </a>
-          </div>
-          <div>
-            <a className="text-teal-500/50" href="#">
-              EN
-            </a>
-          </div>
-        </div>
-        <div>
-          <div>
-            <img className="h-9 mb-10" src="/TT_Logo.svg" alt="logo TT" />
-            <img className="h-28" src="/VCANA_logo.svg" alt="logo VCANA" />
+      <div className="flex space-x-3 absolute top-10 right-3 text-sm font-bold justify-end">
+        <a className="text-teal-500 " href="#">
+          RU
+        </a>
+        <a className="text-teal-500 opacity-50" href="#">
+          EN
+        </a>
+      </div>
+      <div className="flex flex-col items-center m-3 justify-center">
+        <img className="h-9 mb-10" src="/TT_Logo.svg" alt="logo TT" />
+        <img className="h-28" src="/VCANA_logo.svg" alt="logo VCANA" />
 
-            <div className="pt-9 pb-16 text-2xl leading-7 text-zinc-750">
-              {/* <div className="h-9 w-9">
-                <Image
-                  src="/TT_Logo.svg"
-                  alt="Landscape picture"
-                  width={500}
-                  height={500}
-                />
-              </div> */}
-              <span>Вход на данный сайт только по приглашению админов ресурса.</span>
-            </div>
-          </div>
+        <div className="pt-9 pb-16 text-2xl text-center text-zinc-750">
+          Вход на данный сайт только по приглашению админов ресурса.
         </div>
-        <div className="flex justify-center">
-          <div className="btn">ВХОД</div>
-        </div>
+        <div className="btn">ВХОД</div>
       </div>
     </div>
   )
