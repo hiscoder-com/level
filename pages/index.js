ff7e8aa4488a1cf6f3d7ca044b828af9de286748
Pavel <braurs64@gmail.com>
2022-05-06 16:42:30 +0400

    fix text-xl

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="flex absolute top-10 right-10 text-2xl font-bold justify-end">
+      <div className="flex absolute top-10 right-10 text-xl font-bold justify-end">
         <a className="text-teal-500 p-2" href="#">
           RU
         </a>
