215287bba1c88e20fad26dcb9f3038f91240a761
Pavel <braurs64@gmail.com>
2022-05-06 15:04:39 +0400

    fix for netLify

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="flex space-x-3 absolute top-10 right-3 text-sm font-bold justify-end">
-        <a className="text-teal-500 " href="#">
+      <div className="flex absolute top-10 right-10 text-2xl font-bold justify-end">
+        <a className="text-teal-500 p-2" href="#">
           RU
         </a>
-        <a className="text-teal-500 opacity-50" href="#">
+        <a className="text-teal-500 opacity-50 p-2" href="#">
           EN
         </a>
       </div>
@@ -21,7 +21,7 @@ export default function Home() {
         <img className="h-9 mb-10" src="/TT_Logo.svg" alt="logo TT" />
         <img className="h-28" src="/VCANA_logo.svg" alt="logo VCANA" />
 
-        <div className="pt-9 pb-16 text-2xl text-center text-zinc-750">
+        <div className="mt-9 mb-16 text-2xl text-center text-zinc-750">
           Вход на данный сайт только по приглашению админов ресурса.
         </div>
         <div className="btn">ВХОД</div>
