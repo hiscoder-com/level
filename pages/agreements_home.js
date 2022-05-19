34fd51501eb42dad0b740e3df52dc5baddeefa3c
Pavel <braurs64@gmail.com>
2022-05-19 14:07:53 +0400

    creating templates

---
+++ pages/agreements_home.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react'
+import Head from 'next/head'
+
+export default function Agreements_home() {
+  return <div>hello</div>
+}
---
+++ pages/confession_text.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react'
+import Head from 'next/head'
+
+export default function Confession_text() {
+  // return ()
+}
