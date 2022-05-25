238d75bf61dcc69a9ae317859ad99b7fbfbaae67
Pavel <braurs64@gmail.com>
2022-05-25 17:40:58 +0400

    wip on password input

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import Report from '../public/report.svg'
+import EyeIcon from '../public/eyeIcon.svg'
 
 export default function Login() {
   const [errorText, setErrorText] = useState('')
@@ -7,6 +9,7 @@ export default function Login() {
   const [styleLogin, setStyleLogin] = useState('form')
   const [stylePassword, setStylePassword] = useState('form')
   const [formValid, setFormValid] = useState(false)
+  const [passType, setPassType] = useState('text')
 
   const examinationLogPass = () => {
     if (login === 'qwerty' && password === '1234') {
@@ -23,11 +26,17 @@ export default function Login() {
   const report = () => {
     alert('Вы написали админу')
   }
+  const show_hide_password = () => {
+    if (passType == 'password') {
+      setPassType('text')    
+    } else {
+      setPassType('password')    
+    }
+  }
   return (
     <div className="LTAppbar">
       <div>
         <p className="h1 mb-8">Вход:</p>
-        <p>{errorText}</p>
         <form className=" mb-4 space-y-2.5">
           <input
             className={styleLogin}
@@ -40,15 +49,20 @@ export default function Login() {
           />
           <input
             className={stylePassword}
-            type="password"
+            type={passType}
             placeholder="Пароль:"
             onChange={(event) => {
               setPassword(event.target.value)
               setStylePassword('form')
             }}
-          />
+            />
+            <EyeIcon className="absolute" onClick={show_hide_password}/>
         </form>
-        <div className="flex gap-2.5 mt-2 h-9">
+        <div class="flex items-center justify-between">
+        <p className='text-xs text-red-600'>{errorText}</p>
+        <a href="#" class="font-medium text-sky-500 hover:text-sky-600"> Забыли пароль? </a>
+        </div>
+        <div className="flex gap-2.5 mt-5 h-9">
           <button
             disabled={!formValid}
             onClick={report}
