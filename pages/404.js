5c6c8fd3b8e8aab097ee85ab56709e30a9d97c44
Artyom <artchudakov@gmail.com>
2022-06-25 12:45:28 +0200

    some solution for Error Page #63

---
+++ pages/404.js
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import NotFound from '../public/404-error.svg'
+
+const PageNotFound = () => {
+  return (
+    <div className="layout-appbar">
+      <NotFound />
+      <div className="text-xl">
+        <h2>That page cannot be found.</h2>
+        <p>
+          Go back to the{' '}
+          <Link href="/">
+            <a className="text-2xl uppercase text-red-400 hover:text-stone-500">
+              Homepage
+            </a>
+          </Link>
+        </p>
+      </div>
+    </div>
+  )
+}
+export default PageNotFound
