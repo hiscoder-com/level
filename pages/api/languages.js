0bce19cab56bfe717b5c346a9faa4a41df95cdcc
Vitek Foxprogs <foxprogs@gmail.com>
2022-06-27 20:48:03 +0400

    add auth for api requests

--- pages/api/languages.js
+++ pages/api/languages.js
@@ -1,9 +1,13 @@
 import { supabase } from '../../utils/supabaseClient'
 
 export default async function handler(req, res) {
+  if (!req.headers.token) {
+    res.status(401).json({ error: 'Access denied!' })
+  }
+  supabase.auth.setAuth(req.headers.token)
   const { data: languages, error } = await supabase.from('languages').select('*')
   if (error) {
     res.status(404).json({ error })
   }
-  res.status(200).json({ languages })
+  res.status(200).json({ languages, token: req.headers.token })
 }
--- pages/api/users.js
+++ pages/api/users.js
@@ -1,6 +1,10 @@
 import { supabase } from '../../utils/supabaseClient'
 
 export default async function handler(req, res) {
+  if (!req.headers.token) {
+    res.status(401).json({ error: 'Access denied!' })
+  }
+  supabase.auth.setAuth(req.headers.token)
   const { data, error } = await supabase.from('users').select('*')
   if (error) {
     res.status(404).json({ error })
