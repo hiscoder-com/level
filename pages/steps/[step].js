2bc0daf1291d17473dc76c759b0f13625784df83
Pavel <braurs64@gmail.com>
2022-06-28 11:07:46 +0400

    wip

---
+++ pages/steps/[step].js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+
+export default function StepPage() {
+  const { user } = useRouter()
+  return (
+    <div className="container">
+      <Head>
+        <title>V-CANA Step {step}</title>
+        <meta name="description" content="VCANA" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Step: {step}</div>
+    </div>
+  )
+}
