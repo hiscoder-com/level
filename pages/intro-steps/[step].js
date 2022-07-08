6d2651af4c38afdeeff271026e5f58f13ed5e114
Pavel <braurs64@gmail.com>
2022-07-08 19:20:50 +0400

    fix

--- pages/intro-steps/[step].js
+++ pages/intro-steps/[step].js
@@ -18,7 +18,7 @@ export default function IntroPage() {
   )
 }
 
-export async function getServerSideProps({ locale }) {
+export async function getStaticProps({ locale }) {
   return {
     props: {
       ...(await serverSideTranslations(locale, ['intro-steps', 'common'])),
