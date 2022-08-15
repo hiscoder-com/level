2f69bdad6b8e80cb455ff35eed66eaba561f84ce
Vorobusec <timur.yancev08@gmail.com>
2022-08-15 05:40:31 +0400

    WIP

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -2,66 +2,39 @@ import { useEffect, useState } from 'react'
 
 export { useState } from 'react'
 const countStepsNum = 7
-export default function ProgressBar() {
-  const [currentStepNum, setCurrentStepNum] = useState(4)
-  const [currentSteps, setCurrentStep] = useState([])
 
+export default function ProgressBar() {
+  const [currentStepNum, setCurrentStepNum] = useState(5)
+  const [currentSteps, setCurrentStep] = useState([])
   useEffect(() => {
     let steps = []
 
     for (const step = 0; step < countStepsNum; step++) {
       const circle = (
-        <div className={`${step < currentStepNum ? '' : 'zero-activ'} zero`}>
+        <>
+          <div className={`${step < currentStepNum ? 'step active' : 'step'}`}></div>
           <div
-            className={`${
-              step < currentStepNum ? '' : 'progress-step-activ'
-            } progress-step`}
+            className={`${step < currentStepNum - 1 ? 'tire tire-active ' : 'tire'} ${
+              step == countStepsNum - 1 ? 'hidden' : ''
+            }`}
           ></div>
-        </div>
+        </>
       )
       steps.push(circle)
     }
+    const lastStep = steps[steps.length - 1]
+    console.log(lastStep)
     setCurrentStep(steps)
   }, [currentStepNum])
 
   return (
     <>
       <div className="progress-bar">
-        <p className="mr-2 mb-1 ">
+        <p className="mr-2 mb-1 text-2xl ">
           {currentStepNum}/{countStepsNum}:
         </p>
-        {/* <div className="step"></div>
-        <div className="step active"></div> */}
-        <div className="zero">
-          <div className="progress-step"></div>
-        </div>
-        <div className="tire"></div>
-
-        <div className="zero">
-          <div className="progress-step"></div>
-        </div>
-        <div className="tire"></div>
-        <div className="zero">
-          <div className="progress-step"></div>
-        </div>
-        <div className="tire"></div>
-        <div className="zero">
-          <div className="progress-step"></div>
-        </div>
-        <div className="tire tire-activ"></div>
-        <div className="zero zero-activ">
-          <div className="progress-step progress-step-activ"></div>
-        </div>
-        <div className="tire tire-activ"></div>
-        <div className="zero zero-activ">
-          <div className="progress-step progress-step-activ"></div>
-        </div>
-        <div className="tire tire-activ"></div>
-        <div className="zero zero-activ">
-          <div className="progress-step progress-step-activ">7</div>
-        </div>
+        {currentSteps}
       </div>
-      {/* {currentSteps} */}
     </>
   )
 }
