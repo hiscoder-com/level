fae23bc9eb255aa5bc979c4cec5cffd9b0214778
Pavel <braurs64@gmail.com>
2022-08-17 13:07:17 +0400

    fix [lang].js

--- components/Projects.js
+++ components/Projects.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import Link from 'next/link'
 
 import { useProjects, useUserProjects } from '@/utils/hooks'
 import { useCurrentUser } from '../lib/UserContext'
+
+import Fulfilled from '../public/fulfilled.png'
 
 export default function Projects({ languageCode }) {
   const { user } = useCurrentUser()
@@ -18,6 +21,7 @@ export default function Projects({ languageCode }) {
   })
 
   const projects = user?.is_admin ? adminProjects : userProjects
+
   return (
     <>
       <div className="container">
@@ -26,15 +30,38 @@ export default function Projects({ languageCode }) {
           <meta name="description" content="VCANA" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <div>{`${user?.is_admin ? 'Проекты' : 'Мои проекты'}`}</div>
-        {projects &&
-          projects.map((project) => {
-            return (
-              <Link key={project.id} href={`/projects/${project.code}`}>
-                <a className="block text-blue-600">{`${project.id} ${project.title} ${project.code}`}</a>
-              </Link>
-            )
-          })}
+        <div className="text-3xl mb-5">
+          {`${user?.is_admin ? 'Проекты' : 'Мои проекты'}`}:
+        </div>
+        <div className="grid grid-cols-1 gap-7 my-5 sm:grid-cols-1 md:grid-cols-2 md:my-10 xl:grid-cols-3">
+          {projects &&
+            projects.map((project) => {
+              return (
+                <div
+                  key={project.id}
+                  className="block p-6 h-full text-xl bg-white rounded-xl sm:h-52"
+                >
+                  <Link href={`/projects/${project.code}`}>
+                    <a className="block text-2xl mb-4 text-blue-450 underline decoration-2 underline-offset-4">
+                      {project.title}
+                    </a>
+                  </Link>
+                  <div className="flex gap-2.5 mb-1.5">
+                    <p className="text-gray-500">Язык:</p>
+                    <p>{project.languages.orig_name}</p>
+                  </div>
+                  <div className="flex gap-2.5 mb-1.5">
+                    <p className="text-gray-500">Прогресс:</p>
+                    <p>10%</p>
+                  </div>
+                  <div className="flex gap-2.5 text-gray-500">
+                    <p className="text-gray-500">Переводчики:</p>
+                    <Image src={Fulfilled} alt="Fulfilled" width="308" height="64" />
+                  </div>
+                </div>
+              )
+            })}
+        </div>
       </div>
     </>
   )
--- pages/languages/[lang].js
+++ pages/languages/[lang].js
@@ -7,13 +7,22 @@ export default function LanguagesPage() {
   const router = useRouter()
   const { lang } = router.query
   return (
-    <div className="container">
+    <div className="px-5 mx-auto pb-16 lg:max-w-7xl">
       <Head>
         <title>V-CANA languages</title>
         <meta name="description" content="VCANA" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Projects languageCode={lang} />
+
+      <div>
+        <div className="my-10">
+          <Projects languageCode={lang} />
+        </div>
+        <div>
+          <div className="text-3xl leading-9 font-medium mb-5">Мой прогресс:</div>
+          <div>прогресс...</div>
+        </div>
+      </div>
     </div>
   )
 }
