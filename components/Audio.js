7755a6ca31798059181379de68bdbd6f02c38214
Vorobusec <timur.yancev08@gmail.com>
2022-09-26 06:50:05 +0400

    addded trash button, fix fun clear

--- components/Audio.js
+++ components/Audio.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Recorder from './Recorder'
 import { useTranslation } from 'next-i18next'
+
 export default function Audio() {
   const [item, setItem] = useState(true)
 
@@ -8,13 +9,13 @@ export default function Audio() {
     <>{item ? <AudioList setItem={setItem} /> : <AudioRecorder setItem={setItem} />}</>
   )
 }
+
 function AudioList({ setItem }) {
-  const { t } = useTranslation(['common', 'steps'])
+  const { t } = useTranslation(['common'])
   return (
     <>
-      <div className="layout-step-col-card-body-audio">
+      <div className="flex flex-col items-center gap-5 py-24 lg:py-56 h5">
         <p>{t('AudioTitle')}</p>
-
         <button
           onClick={() => {
             setItem(false)
@@ -27,42 +28,19 @@ function AudioList({ setItem }) {
     </>
   )
 }
-function AudioRecorder({}) {
-  const [microphoneAccess, setMicrophoneAccess] = useState(false)
-  const [clear, setClear] = useState(false)
-  const [recording, setRecording] = useState(false)
+
+function AudioRecorder() {
+  const { t } = useTranslation(['common'])
   return (
-    <div className="flex justify-center flex-wrap mt-8  ">
-      <p className={`text-center font-bold mb-4 ${microphoneAccess ? '' : 'hidden'}`}>
-        Приложению V-Cana нужен доступ к микрофону. Нажмите на значок заблокированной
-        камеры(микрофона) в адресной строке. Разрешите доступ и перезагрузите страницу.
-      </p>
+    <div className="flex justify-center flex-wrap mt-8">
       <div className="pb-4 mb-4 border-b-4">
-        <p className="mb-4">Пересказ на исходном языке:</p>
-        <Recorder
-          recording={recording}
-          setRecording={setRecording}
-          clear={clear}
-          setClear={setClear}
-          setMicrophoneAccess={setMicrophoneAccess}
-        />
+        <p className="mb-4">{t('OriginalRecording')}</p>
+        <Recorder />
       </div>
       <div className="pb-4 mb-4 border-b-4">
-        <p className="mb-4">Пересказ на целевом языке:</p>
-        <Recorder
-          clear={clear}
-          setClear={setClear}
-          setMicrophoneAccess={setMicrophoneAccess}
-        />
+        <p className="mb-4">{t('TargetRecording')}</p>
+        <Recorder />
       </div>
-      <button
-        className="btn-cyan"
-        onClick={() => {
-          setClear(true)
-        }}
-      >
-        Очистить
-      </button>
     </div>
   )
 }
--- components/Recorder.js
+++ components/Recorder.js
@@ -1,14 +1,24 @@
-import { useRef, useEffect, useState, useCallback } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { useRef, useEffect, useState, Fragment } from 'react'
+
+import { useTranslation } from 'next-i18next'
+
 import RecorderButton from '../public/recorder.svg'
 import StopButton from '../public/stop.svg'
+import RecorderCrossedButton from '../public/recorder-crossed.svg'
+import TrashButton from '../public/trash.svg'
 
-export default function Recorder({ clear, setClear, setMicrophoneAccess }) {
+export default function Recorder() {
+  const { t } = useTranslation(['common'])
+  const [clear, setClear] = useState(false)
+  const [showModal, setShowModal] = useState(false)
   const audioRef = useRef(null)
   const [mediaRec, setMediaRec] = useState()
   const [voice, setVoice] = useState([])
   const [button, setButton] = useState(
     <RecorderButton className="stroke-cyan-700 stroke-2" />
   )
+
   const startStop = () => {
     if (mediaRec?.state === 'inactive') {
       setVoice([])
@@ -18,10 +28,10 @@ export default function Recorder({ clear, setClear, setMicrophoneAccess }) {
       mediaRec.stop()
       setButton(<RecorderButton className="stroke-cyan-700 stroke-2" />)
     } else {
-      setMicrophoneAccess(true)
-      alert('Доступ к микрофону отсутствует')
+      setShowModal(true)
     }
   }
+
   useEffect(() => {
     navigator.mediaDevices
       .getUserMedia({ audio: true })
@@ -33,6 +43,7 @@ export default function Recorder({ clear, setClear, setMicrophoneAccess }) {
         setMediaRec(mediaRecorder)
       })
       .catch((err) => {
+        setButton(<RecorderCrossedButton className="stroke-red-700 stroke-1" />)
         console.error(`Вы не дали доступ к микрофону: ${err}`)
       })
   }, [])
@@ -41,19 +52,99 @@ export default function Recorder({ clear, setClear, setMicrophoneAccess }) {
       setClear(false)
       setVoice([])
     }
-    if (voice) {
+  }, [clear])
+
+  useEffect(() => {
+    if (voice.length > 0) {
       const blobUrl = window.URL.createObjectURL(
         new Blob(voice, { type: 'audio/webm;codecs=opus' })
       )
       audioRef.current.src = blobUrl
     }
-  }, [voice, clear, setClear])
+  }, [voice])
+
   return (
     <div className="flex justify-center items-center">
       <audio className="mr-2" ref={audioRef} controls></audio>
-      <button className="border-0 w-6 h-6" onClick={startStop}>
+      <button className="border-0 w-6 h-6 mr-2" onClick={startStop}>
         {button}
       </button>
+      <br />
+      <button
+        disabled={voice.length > 0 ? '' : 'disabled'}
+        className="border-0 w-6 h-6"
+        onClick={() => {
+          setClear(true)
+        }}
+      >
+        <TrashButton
+          className={`stroke-2 ${
+            voice.length > 0 ? 'stroke-cyan-700' : 'stroke-gray-300'
+          }`}
+        />
+      </button>
+      <Modal showModal={showModal} setShowModal={setShowModal} />
     </div>
+  )
+}
+
+function Modal({ showModal, setShowModal }) {
+  const { t } = useTranslation(['common'])
+  return (
+    <Transition appear show={showModal} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-10"
+        onClose={() => {
+          setShowModal(false)
+        }}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex items-center justify-center p-4 min-h-full text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="modal-step-goal">
+                <Dialog.Title as="h3" className="h3 font-medium leading-6">
+                  {t('MicrophoneAccess')}
+                </Dialog.Title>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">{t('TurnMicrofon')}</p>
+                </div>
+
+                <div className="mt-4">
+                  <button
+                    className="btn-cyan w-24"
+                    onClick={() => {
+                      setShowModal(false)
+                    }}
+                  >
+                    {t('Accept')}
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
   )
 }
