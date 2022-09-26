d6c3fc8a321308a52cc3b03f7facfc5967d03c77
Vitek Foxprogs <foxprogs@gmail.com>
2022-09-26 14:52:23 +0400

    wip show created chapters

--- pages/projects/[code]/books/[bookid]/index.js
+++ pages/projects/[code]/books/[bookid]/index.js
@@ -10,6 +10,7 @@ function BookChaptersPage() {
   const [project, setProject] = useState()
   const [book, setBook] = useState()
   const [chapters, setChapters] = useState([])
+  const [createdChapters, setCreatedChapters] = useState([])
 
   useEffect(() => {
     const getProject = async () => {
@@ -41,21 +42,6 @@ function BookChaptersPage() {
   }, [bookid, project?.id])
 
   useEffect(() => {
-    const getCreatedChapters = async () => {
-      const { data: chapters, error } = await supabase
-        .from('chapters')
-        .select('id,code,chapters')
-        .eq('project_id', project.id)
-        .eq('book_id', book.id)
-        .single()
-      setChapters(chapters)
-    }
-    if (project?.id && book?.id) {
-      getCreatedChapters()
-    }
-  }, [book?.id, project?.id])
-
-  useEffect(() => {
     const getChapters = async () => {
       const { data: chapters, error } = await supabase
         .from('chapters')
@@ -68,15 +54,38 @@ function BookChaptersPage() {
       getChapters()
     }
   }, [book?.id, project?.id])
-  console.log(project, book, chapters)
+
+  useEffect(() => {
+    const getCreatedChapters = async () => {
+      const { data: createdChaptersRaw, error } = await supabase
+        .from('verses')
+        .select('chapter_id')
+        .eq('project_id', project.id)
+        .in(
+          'chapter_id',
+          chapters.map((el) => el.id)
+        )
+      const createdChapters = new Set(createdChaptersRaw.map((el) => el.chapter_id))
+      setCreatedChapters([...createdChapters])
+    }
+    if (project?.id && chapters?.length) {
+      getCreatedChapters()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [chapters?.length, project?.id])
+
   return (
     <>
       <h2>Project {project?.code}: Book</h2>
       <h3>Book: {book?.code}</h3>
       {chapters?.map((chapter) => (
         <div key={chapter.id}>
-          {chapter.num}:{chapter.verses}
-          <div onClick={() => handleCreate(chapter.id)}>Create</div>
+          {chapter.num}:{chapter.verses} {console.log('1', createdChapters)}
+          {!createdChapters.includes(chapter.id) ? (
+            <div onClick={() => handleCreate(chapter.id)}>Create</div>
+          ) : (
+            <div>Created...</div>
+          )}
         </div>
       ))}
     </>
