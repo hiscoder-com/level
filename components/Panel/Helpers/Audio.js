6d901255677a81b1a5d39c69ac3a6af08853421d
Vorobusec <timur.yancev08@gmail.com>
2022-10-19 12:25:33 +0400

    added retell partner state

--- components/Panel/Helpers/Audio.js
+++ components/Panel/Helpers/Audio.js
@@ -3,31 +3,41 @@ import { useState } from 'react'
 import { useTranslation } from 'next-i18next'
 
 import Recorder from '../../Recorder'
+import BackButton from 'public/back-button.svg'
 
 export default function Audio() {
-  const [isRetellPartner, setIsRetellPartner] = useState(true)
+  const [audioState, setAudioState] = useState('Main Audio')
 
   return (
     <>
-      {isRetellPartner ? (
-        <RetellPartner setIsRetellPartner={setIsRetellPartner} />
+      {audioState === 'Retell Yourself' ? (
+        <RetellYourself setAudioState={setAudioState} />
+      ) : audioState === 'Retell Partner' ? (
+        <RetellPartner setAudioState={setAudioState} />
       ) : (
-        <RetellYourself />
+        <MainAudio setAudioState={setAudioState} />
       )}
     </>
   )
 }
 
-function RetellPartner({ setIsRetellPartner }) {
+function MainAudio({ setAudioState }) {
   const { t } = useTranslation(['audio'])
   return (
     <div className="flex flex-col items-center gap-5 py-24 lg:py-56">
-      <button className="btn-cyan">{t('RetellPartner')}</button>
+      <button
+        onClick={() => {
+          setAudioState('Retell Partner')
+        }}
+        className="btn-cyan"
+      >
+        {t('RetellPartner')}
+      </button>
       <p>{t('Title')}</p>
 
       <button
         onClick={() => {
-          setIsRetellPartner(false)
+          setAudioState('Retell Yourself')
         }}
         className="btn-cyan"
       >
@@ -37,18 +47,51 @@ function RetellPartner({ setIsRetellPartner }) {
   )
 }
 
-function RetellYourself() {
+function RetellPartner({ setAudioState }) {
   const { t } = useTranslation(['audio'])
   return (
-    <div className="flex justify-center flex-wrap mt-8">
-      <div className="w-full pb-4 px-2 mb-4 border-b-4">
-        <p className="mb-4 px-4">{t('OriginalRecording')}</p>
-        <Recorder />
+    <>
+      <button
+        onClick={() => {
+          setAudioState('Main Audio')
+        }}
+        className="border-0 w-4 h-4"
+      >
+        <BackButton className="stroke-cyan-700" />
+      </button>
+      <div className="flex flex-col items-center gap-5 py-24 lg:py-56">
+        <p>{t('StartRetelling')}</p>
+        <div className="flex">
+          <button className="btn-cyan mr-2">{t('InOriginalLanguage')}</button>
+          <button className="btn-cyan">{t('InTargetLanguage')}</button>
+        </div>
       </div>
-      <div className="w-full pb-4 px-2 mb-4 border-b-4">
-        <p className="mb-4 px-4">{t('TargetRecording')}</p>
-        <Recorder />
+    </>
+  )
+}
+
+function RetellYourself({ setAudioState }) {
+  const { t } = useTranslation(['audio'])
+  return (
+    <>
+      <button
+        onClick={() => {
+          setAudioState('Main Audio Component')
+        }}
+        className="border-0 w-4 h-4"
+      >
+        <BackButton className="stroke-cyan-700" />
+      </button>
+      <div className="flex justify-center flex-wrap mt-8">
+        <div className="w-full pb-4 px-2 mb-4 border-b-4">
+          <p className="mb-4 px-4">{t('OriginalRecording')}</p>
+          <Recorder />
+        </div>
+        <div className="w-full pb-4 px-2 mb-4 border-b-4">
+          <p className="mb-4 px-4">{t('TargetRecording')}</p>
+          <Recorder />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
