ecbd6ca2530bbd7ef6e87999934895b14ddd4321
Vitek Foxprogs <foxprogs@gmail.com>
2022-10-24 21:55:49 +0400

    fix translation

--- pages/projects/create.js
+++ pages/projects/create.js
@@ -1,7 +1,17 @@
 import ProjectCreate from 'components/ProjectCreate'
 
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
 function ProjectCreatePage() {
   return <ProjectCreate />
 }
 
 export default ProjectCreatePage
+
+export async function getServerSideProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['projects', 'common'])),
+    },
+  }
+}
