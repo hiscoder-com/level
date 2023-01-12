b5af1376a3cc66dfccdbdeed8007e4e2ca83b9b5
Pavel <braurs64@gmail.com>
2023-01-12 12:20:40 +0400

    fix translate

--- components/EditBrief.js
+++ components/EditBrief.js
@@ -57,17 +57,75 @@ function EditBrief({ user, projectId }) {
       >
         <div className="text-center flex flex-row gap-4 my-6">
           <div className="w-1/3">
-            <p className="mb-2">{t('Questions')}</p>
-            <textarea
+            <p className="mb-2 font-bold text-lg text-gray-500">{t('Questions')}</p>
+            {/* <textarea
               className="border-2 rounded-md p-2 text-gray-500 outline-none w-full h-[69vh]"
               placeholder={t('project-edit:QuestionsPlaceholder')}
               onChange={(e) => setQuestionsText(e.target.value)}
               value={questionsText}
               readOnly
-            />
+            /> */}
+            <div className="border-2 overflow-auto divide-y rounded-md p-2 text-gray-500 w-full h-[69vh]">
+              <div className="text-left pb-3">
+                <p className="font-bold mb-1">1. О языке</p>
+                <p>- как называется язык?</p>
+                <p>- какое межд.сокращение для языка?</p>
+                <p>- где распространён?</p>
+                <p>- почему выбран именно этот язык или диалект?</p>
+                <p>- какой алфавит используется в данном языке?</p>
+              </div>
+              <div className="text-left py-3">
+                <p className="font-bold mb-1">2. О необходимости перевода</p>
+                <p>- почему нужен этот перевод?</p>
+                <p>- какие переводы уже есть на этом языке?</p>
+                <p>
+                  - какие диалекты или другие языки могли бы пользоваться этим переводом?
+                </p>
+                <p>
+                  - как вы думаете могут ли возникнуть трудности с другими командами, уже
+                  работающими над переводом библейского контента на этот язык?
+                </p>
+              </div>
+              <div className="text-left py-3">
+                <p className="font-bold mb-1">3. О целевой аудитории перевода</p>
+                <p>- кто будет пользоваться переводом?</p>
+                <p>- на сколько человек в данной народности рассчитан этот перевод?</p>
+                <p>
+                  - какие языки используют постоянно эти люди, кроме своего родного языка?
+                </p>
+                <p>
+                  - в этой народности больше мужчин/женщин, пожилых/молодых,
+                  грамотных/неграмотных?
+                </p>
+              </div>
+              <div className="text-left py-3">
+                <p className="font-bold mb-1">4. О стиле перевода</p>
+                <p>
+                  - какой будет тип перевода, смысловой или подстрочный (дословный,
+                  буквальный)?
+                </p>
+                <p>- какой будет стиль языка у перевода?</p>
+                <p>- как будет распространяться перевод?</p>
+              </div>
+              <div className="text-left py-3">
+                <p className="font-bold mb-1">5. О команде</p>
+                <p>
+                  - кто инициаторы перевода (кто проявил интерес к тому, чтобы начать
+                  работу над переводом)?
+                </p>
+                <p>- кто будет работать над переводом?</p>
+              </div>
+              <div className="text-left pt-3">
+                <p className="font-bold mb-1">6. О качестве перевода</p>
+                <p>- кто будет оценивать перевод?</p>
+                <p>- как будет поддерживаться качество перевода?</p>
+              </div>
+            </div>
           </div>
           <div className="w-1/3">
-            <p className="mb-2">{t('project-edit:Answers')}</p>
+            <p className="mb-2 font-bold text-lg text-gray-500">
+              {t('project-edit:Answers')}
+            </p>
             <textarea
               className="border-2 rounded-md p-2 text-gray-500 outline-none w-full h-[69vh]"
               placeholder={t('project-edit:AnswersPlaceholder')}
@@ -76,7 +134,9 @@ function EditBrief({ user, projectId }) {
             />
           </div>
           <div className="w-1/3">
-            <p className="mb-2">{t('project-edit:Summary')}</p>
+            <p className="mb-2 font-bold text-lg text-gray-500">
+              {t('PurposeTranslation')}
+            </p>
             <textarea
               className="border-2 rounded-md p-2 text-gray-500 outline-none w-full h-[69vh]"
               placeholder={t('project-edit:SummaryPlaceholder')}
