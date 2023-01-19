a32068dcda6098aa8180dc65ee06f6703b25a045
Pavel <braurs64@gmail.com>
2023-01-19 15:39:07 +0400

    wip on onChange in resume

--- components/EditBrief.js
+++ components/EditBrief.js
@@ -9,7 +9,6 @@ import { useBrief } from 'utils/hooks'
 function EditBrief({ user, projectId }) {
   const [showModalTranslationGoal, setShowModalTranslationGoal] = useState(false)
   const [questionsArray, setQuestionsArray] = useState('')
-  const [summaryText, setSummaryText] = useState('')
 
   const { t } = useTranslation(['common', 'project-edit'])
   const [brief, { mutate }] = useBrief({
@@ -19,7 +18,6 @@ function EditBrief({ user, projectId }) {
 
   useEffect(() => {
     setQuestionsArray(brief?.questions)
-    setSummaryText(brief?.summary)
   }, [brief])
 
   const saveToDatabase = () => {
@@ -27,7 +25,6 @@ function EditBrief({ user, projectId }) {
     axios
       .put(`/api/briefs/${projectId}`, {
         questions: questionsArray,
-        summary: summaryText,
       })
       .then(() => mutate())
       .catch((err) => console.log(err))
@@ -36,9 +33,7 @@ function EditBrief({ user, projectId }) {
   const closeModal = () => {
     setShowModalTranslationGoal(false)
   }
-
-  // console.log(questionsArray[0].title)
-
+  console.log(questionsArray)
   return (
     <>
       <button
@@ -55,112 +50,156 @@ function EditBrief({ user, projectId }) {
         closeHandle={closeModal}
       >
         {questionsArray && (
-          // <div className="text-center flex flex-row gap-4 my-6">
-          //   <div className="w-1/3">
-          //     <p className="mb-2 font-bold text-lg text-gray-500">{t('Questions')}</p>
-          //     <div className="border-2 overflow-auto divide-y rounded-md p-2 text-gray-500 w-full h-[69vh]">
-          //       <div className="text-left pb-3">
-          //         <p className="font-bold mb-3">{`${questionsArray[0].id}. ${questionsArray[0].title}`}</p>
-          //         <div className="h-20">{questionsArray[0].block[2].question}</div>
-          //         <div className="mt-2 h-20">{questionsArray[0].block[1].question}</div>
-          //       </div>
-          //
-          //   <div className="w-1/3">
-          //     <p className="mb-2 font-bold text-lg text-gray-500">
-          //       {t('project-edit:Answers')}
-          //     </p>
-
-          //     <div className="border-2 overflow-auto divide-y rounded-md p-2 text-gray-500 w-full h-[69vh]">
-          //       <div className="text-left pb-3">
-          //         <p className="font-bold mb-1">{`${questionsArray[0].id}. ${questionsArray[0].title}`}</p>
-          //         <textarea
-          //           className="p-2 outline-none w-full h-20"
-          //           value={questionsArray[0].block[2].answer}
-          //           // onChange={(e) => {
-          //           //   setQuestionsArray((prev) => ({
-          //           //     ...prev,
-          //           //     question1: {
-          //           //       ...prev[0],
-          //           //       answers: e.target.value.split('\n'),
-          //           //     },
-
-          //           //     question1: {
-          //           //       ...prev.question1,
-          //           //       answers: e.target.value.split('\n'),
-          //           //     },
-          //           //   }))
-          //           // }}
-          //         />
-          //         <textarea
-          //           className="p-2 outline-none w-full h-28"
-          //           value={questionsArray[0].block[1].answer}
-          //         />
-          //       </div>
-          //       {/* <div className="text-left py-3">
-          //         <p className="font-bold mb-1">
-          //           {`${questionsArray[0].id}. ${questionsArray[1].title}`}
-          //         </p>
-          //         <textarea
-          //           className="p-2 outline-none w-full h-40"
-          //           // value={questionsArray?.question2?.answers.join('\n')}
-          //           // onChange={(e) => {
-          //           //   setQuestionsArray((prev) => ({
-          //           //     ...prev,
-          //           //     question2: {
-          //           //       ...prev.question2,
-          //           //       answers: e.target.value.split('\n'),
-          //           //     },
-          //           //   }))
-          //           // }}
-          //         />
-          //   <div className="w-1/3">
-          //     <p className="mb-2 font-bold text-lg text-gray-500">
-          //       {t('PurposeTranslation')}
-          //     </p>
-          //     <textarea
-          //       className="border-2 rounded-md p-2 text-gray-500 outline-none w-full h-[69vh]"
-          //       placeholder={t('project-edit:SummaryPlaceholder')}
-          //       onChange={(e) => setSummaryText(e.target.value)}
-          //       value={summaryText}
-          //     />
-          //   </div>
-          // </div>
-          <div className="w-full h-[69vh]">
-            <table className="table-fixed border-spacing-x-4 w-full border-separate border-spacing-y-0 my-6 text-gray-500">
+          // <div className="w-full h-[69vh]">
+          <div className="w-full">
+            <table className="table-fixed border-b-4 w-full my-6 text-gray-500">
               <thead className="font-bold text-lg">
                 <tr>
-                  <th className="pb-2 w-1\3">{t('Questions')}</th>
-                  <th className="pb-2 w-1\3">{t('project-edit:Answers')}</th>
-                  <th className="pb-2 w-1\3">{t('PurposeTranslation')}</th>
+                  <th className="pb-2 w-1\3 border-b-4">{t('Questions')}</th>
+                  <th className="pb-2 w-1\3 border-b-4">{t('project-edit:Answers')}</th>
+                  <th className="pb-2 w-1\3 border-b-4">{t('PurposeTranslation')}</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td className="border p-2 font-bold">{`${questionsArray[0].id}. ${questionsArray[0].title}`}</td>
-                  <td className="border p-2 font-bold">{`${questionsArray[0].id}. ${questionsArray[0].title}`}</td>
-                  <td rowSpan={6} className="border p-2">
-                    Перевод будет сделан на азербайджанский язык, на латинице
+                  <td className="border p-2 font-bold border-x-4">{`${questionsArray[0].id}. ${questionsArray[0].title}`}</td>
+                  <td className="border p-2 font-bold border-x-4">{`${questionsArray[0].id}. ${questionsArray[0].title}`}</td>
+                  <td rowSpan={6} className="p-2 border-b-4 border-x-4 text-center">
+                    <textarea
+                      value={questionsArray[0].resume}
+                      onChange={(e) => {
+                        setQuestionsArray((prev) => ({
+                          ...prev,
+                          // здесь объект, а нужно попасть в массив
+                          [0]: {
+                            ...prev[0],
+                            resume: e.target.value,
+                          },
+                        }))
+                      }}
+                      className="p-2 outline-none w-full h-full"
+                    />
                   </td>
                 </tr>
                 <tr>
-                  <td className="border p-2">{questionsArray[0].block[0].question}</td>
-                  <td className="border p-2">{questionsArray[0].block[0].answer}</td>
+                  <td className="border-x-4 p-2">
+                    {questionsArray[0].block[0].question}
+                  </td>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[0].block[0].answer}
+                  </td>
                 </tr>
                 <tr>
-                  <td className="border p-2">{questionsArray[0].block[1].question}</td>
-                  <td className="border p-2">{questionsArray[0].block[1].answer}</td>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[0].block[1].question}
+                  </td>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[0].block[1].answer}
+                  </td>
                 </tr>
                 <tr>
-                  <td className="border p-2">{questionsArray[0].block[2].question}</td>
-                  <td className="border p-2">{questionsArray[0].block[2].answer}</td>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[0].block[2].question}
+                  </td>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[0].block[2].answer}
+                  </td>
                 </tr>
                 <tr>
-                  <td className="border p-2">{questionsArray[0].block[3].question}</td>
-                  <td className="border p-2">{questionsArray[0].block[3].answer}</td>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[0].block[3].question}
+                  </td>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[0].block[3].answer}
+                  </td>
                 </tr>
                 <tr>
-                  <td className="border p-2">{questionsArray[0].block[4].question}</td>
-                  <td className="border p-2">{questionsArray[0].block[4].answer}</td>
+                  <td className="border border-x-4 border-b-2 p-2">
+                    {questionsArray[0].block[4].question}
+                  </td>
+                  <td className="border border-x-4 border-b-2 p-2">
+                    {questionsArray[0].block[4].answer}
+                  </td>
+                </tr>
+
+                <tr>
+                  <td className="border p-2 font-bold border-t-4 border-x-4">{`${questionsArray[1].id}. ${questionsArray[1].title}`}</td>
+                  <td className="border p-2 font-bold border-t-4 border-x-4">{`${questionsArray[1].id}. ${questionsArray[1].title}`}</td>
+                  <td rowSpan={5} className="p-2 border-b-4 border-x-4 text-center">
+                    {questionsArray[1].resume}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border-x-4 p-2">
+                    {questionsArray[1].block[0].question}
+                  </td>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[1].block[0].answer}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[1].block[1].question}
+                  </td>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[1].block[1].answer}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[1].block[2].question}
+                  </td>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[1].block[2].answer}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border border-x-4 border-b-2 p-2">
+                    {questionsArray[1].block[3].question}
+                  </td>
+                  <td className="border border-x-4 border-b-2 p-2">
+                    {questionsArray[1].block[3].answer}
+                  </td>
+                </tr>
+
+                <tr>
+                  <td className="border p-2 font-bold border-t-4 border-x-4">{`${questionsArray[2].id}. ${questionsArray[2].title}`}</td>
+                  <td className="border p-2 font-bold border-t-4 border-x-4">{`${questionsArray[2].id}. ${questionsArray[2].title}`}</td>
+                  <td rowSpan={6} className="p-2 border-x-4 text-center">
+                    {questionsArray[2].resume}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border-x-4 p-2">
+                    {questionsArray[2].block[0].question}
+                  </td>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[2].block[0].answer}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[2].block[1].question}
+                  </td>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[2].block[1].answer}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[2].block[2].question}
+                  </td>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[2].block[2].answer}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[2].block[3].question}
+                  </td>
+                  <td className="border border-x-4 p-2">
+                    {questionsArray[2].block[3].answer}
+                  </td>
                 </tr>
               </tbody>
             </table>
--- pages/api/briefs/[id].js
+++ pages/api/briefs/[id].js
@@ -7,7 +7,7 @@ export default async function briefsGetHandler(req, res) {
   supabase.auth.setAuth(req.headers.token)
   const {
     query: { id },
-    body: { questions, answers, summary },
+    body: { questions },
     method,
   } = req
 
@@ -31,7 +31,7 @@ export default async function briefsGetHandler(req, res) {
       try {
         const { data, error } = await supabase
           .from('briefs')
-          .update({ questions, answers, summary })
+          .update({ questions })
           .match({ project_id: id })
         if (error) throw error
         res.status(200).json(data)
