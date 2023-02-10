39593d38d59cd0c47285560bac39c1f1f2c17bef
Pavel <braurs64@gmail.com>
2023-02-10 22:40:41 +0400

    wip on validation

--- pages/api/briefs/[id].js
+++ pages/api/briefs/[id].js
@@ -11,20 +11,34 @@ export default async function briefsGetHandler(req, res) {
     method,
   } = req
 
-  const validation = (properties) => {
-    const error = null
-    if (!properties) {
+  const validation = (brief_data) => {
+    if (!brief_data) {
       return { error: 'Properties is null or undefined' }
     }
-
-    if (
-      JSON.stringify(Object.keys(properties[0]).sort()) !==
-      JSON.stringify(['block', 'id', 'resume', 'title'].sort())
-    ) {
-      return { error: 'Properties has different keys', properties }
+    if (Array.isArray(brief_data)) {
+      const isValidKeys = brief_data.find((item) => {
+        const isNotValid =
+          JSON.stringify(Object.keys(item).sort()) !==
+          JSON.stringify(['block', 'id', 'resume', 'title'].sort())
+        if (isNotValid) {
+          return isNotValid
+        } else {
+          item.block?.forEach((element) => {
+            if (
+              JSON.stringify(Object.keys(element).sort()) !==
+              JSON.stringify(['question', 'answer'].sort())
+            ) {
+              return { error: 'test error!' } //
+            }
+          })
+        }
+      })
+      if (isValidKeys) {
+        return { error: 'Properties has different keys', isValidKeys }
+      }
     }
 
-    return { error }
+    return { error: null }
   }
 
   switch (method) {
@@ -45,13 +59,15 @@ export default async function briefsGetHandler(req, res) {
       break
     case 'PUT':
       try {
-        if (data_collection?.length > 1 && validation(data_collection).error === null) {
+        if (data_collection?.length > 1 && !validation(data_collection)?.error) {
           const { data, error } = await supabase
             .from('briefs')
             .update({ data_collection })
             .match({ project_id: id })
           if (error) throw error
           res.status(200).json(data)
+        } else {
+          res.status(404).json({ error })
         }
       } catch (error) {
         res.status(404).json({ error })
