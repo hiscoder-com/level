75772bf7363c548dea14423a8734b1f335943b76
Pavel <braurs64@gmail.com>
2023-02-20 16:38:29 +0400

    added btn animate

--- components/EditBrief.js
+++ components/EditBrief.js
@@ -12,6 +12,7 @@ import { supabase } from 'utils/supabaseClient'
 
 function EditBrief() {
   const [briefDataCollection, setBriefDataCollection] = useState('')
+  const [isSaving, setIsSaving] = useState(false)
   const [level, setLevel] = useState('user')
   const highLevelAccess = ['admin', 'coordinator'].includes(level)
 
@@ -33,6 +34,8 @@ function EditBrief() {
   }, [brief])
 
   const saveToDatabase = () => {
+    setIsSaving(true)
+
     axios.defaults.headers.common['token'] = user?.access_token
     axios
       .put(`/api/briefs/${project?.id}`, {
@@ -40,6 +43,9 @@ function EditBrief() {
       })
       .then(() => mutate())
       .catch((err) => console.log(err))
+      .finally(() => {
+        setIsSaving(false)
+      })
   }
 
   useEffect(() => {
@@ -203,8 +209,31 @@ function EditBrief() {
           )}
           {highLevelAccess && (
             <div className="flex justify-center">
-              <button className="btn-cyan" onClick={saveToDatabase}>
-                {t('Save')}
+              <button className="btn-cyan" onClick={saveToDatabase} disabled={isSaving}>
+                {isSaving ? (
+                  <svg
+                    className="animate-spin my-0 mx-auto h-5 w-5 text-blue-600"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                ) : (
+                  t('Save')
+                )}
               </button>
             </div>
           )}
