e11616d75f46da231acf197933fdbe5ba33f10e7
Pavel <braurs64@gmail.com>
2023-03-13 14:23:29 +0400

    fix user-agreement.json

--- pages/agreements.js
+++ pages/agreements.js
@@ -4,13 +4,13 @@ import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 export default function Agreements() {
-  const { t } = useTranslation('common')
+  const { t } = useTranslation('common', 'users')
 
   return (
     <div className="layout-appbar">
       <div className="flex flex-col text-center space-y-2.5">
         <Link href="/user-agreement">
-          <a className="btn-white w-64">{t('Agreements')}</a>
+          <a className="btn-white w-64">{t('users:Agreement')}</a>
         </Link>
         <Link href="/confession">
           <a className="btn-white w-64">{t('ConfessionFaith')}</a>
