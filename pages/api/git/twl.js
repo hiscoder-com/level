e47285a338dc0e5e3228d71efd5719e17046a286
valyukhov <alexzed@bk.ru>
2023-04-03 16:15:41 +0300

    feat: :sparkles: change in obs-twl

--- pages/api/git/twl.js
+++ pages/api/git/twl.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 import { tsvToJson, markRepeatedWords } from '@texttree/translation-words-helpers'
-import { uniqueFilterInBook, getListWordsReference } from 'utils/helper'
 
 /**
  *  @swagger
