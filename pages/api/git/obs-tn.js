519a6db08c06ca19f0adb90f58be559d99dac423
valyukhov <alexzed@bk.ru>
2023-04-03 17:16:21 +0300

    refactor: :fire: remove tsvHelper.js

--- pages/api/git/obs-tn.js
+++ pages/api/git/obs-tn.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-import { filterNotes } from 'utils/tsvHelper'
+import { filterNotes } from 'utils/helper'
 import { tsvToJson } from '@texttree/translation-words-helpers'
 
 /**
--- utils/tsvHelper.js
+++
@@ -1,7 +0,0 @@
-export function filterNotes(newNote, verse, notes) {
-  if (!notes[verse]) {
-    notes[verse] = [newNote]
-  } else {
-    notes[verse].push(newNote)
-  }
-}
