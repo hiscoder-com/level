a83fea2621006a35d6904ab971a4deaa4ad0b960
valyukhov <alexzed@bk.ru>
2023-04-13 22:27:02 +0300

    feat: :sparkles: update design for Account

--- components/Account.js
+++ components/Account.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import { useRouter } from 'next/router'
 
@@ -7,10 +7,12 @@ import { useTranslation } from 'next-i18next'
 import Projects from 'components/Projects'
 
 import { useCurrentUser } from 'lib/UserContext'
+import PersonalList from './PersonalList'
 
 function Account() {
   const { user, loading } = useCurrentUser()
   const router = useRouter()
+  const [accountType, setAccountType] = useState('account')
 
   const { t } = useTranslation(['users'])
 
@@ -22,19 +24,31 @@ function Account() {
 
   return (
     <div className="mx-auto max-w-7xl">
-      <h1 className="h1">{t('Account')}</h1>
-
       {user?.id && (
-        <>
-          <p className="mt-3">
-            {t('Login')}: <b>{user.login}</b>
-          </p>
-          <p>
-            {t('Email')}: <b>{user.email}</b>
-          </p>
-
-          <Projects />
-        </>
+        <div className="divide-y divide-darkBlue">
+          <div className="grid grid-cols-2 gap-7  md:grid-cols-3 md:mt-24 xl:grid-cols-5 md:text-xl font-bold text-center">
+            <button
+              disabled={accountType === 'account'}
+              onClick={() => setAccountType('account')}
+              className={accountType === 'account' ? 'tab-active' : 'tab cursor-pointer'}
+            >
+              {t('Account')}
+            </button>
+            <button
+              disabled={accountType === 'projects'}
+              onClick={() => setAccountType('projects')}
+              className={
+                accountType === 'projects' ? 'tab-active ' : 'tab cursor-pointer'
+              }
+            >
+              {t('Projects')}
+            </button>
+          </div>
+          <div>
+            {accountType === 'account' && <PersonalList />}
+            {accountType === 'projects' && <Projects />}
+          </div>
+        </div>
       )}
     </div>
   )
