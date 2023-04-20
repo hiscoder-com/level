bb66d02c97ee80daacf083d40d252bd1ba0134e3
valyukhov <alexzed@bk.ru>
2023-04-20 18:28:34 +0300

    feat: :sparkles: add support of CheckingIcon from chapters

--- components/Project/ChecksIcon.js
+++ components/Project/ChecksIcon.js
@@ -1,6 +1,26 @@
+import { useMemo } from 'react'
+import { useGetChapters, useGetCreatedChapters } from 'utils/hooks'
 import Checking from '../../public/checking.svg'
 
-function ChecksIcon({ checktype }) {
+function ChecksIcon({ book, user, project }) {
+  const [chapters] = useGetChapters({
+    token: user?.access_token,
+    code: project?.code,
+    book_code: book,
+  })
+  const [createdChapters] = useGetCreatedChapters({
+    token: user?.access_token,
+    code: project?.code,
+    chapters: chapters?.map((el) => el.id),
+  })
+  const checktype = useMemo(
+    () =>
+      createdChapters?.length &&
+      chapters?.filter((el) => el.finished_at)?.length === chapters?.length
+        ? 'first-check'
+        : '',
+    [chapters, createdChapters?.length]
+  )
   const checks = ['first-check', 'second-check', 'third-check'].filter((el) => {
     switch (checktype) {
       case 'first-check':
