1d813b11066e875586c8c8d3c96feb18161bc5bd
valyukhov <alexzed@bk.ru>
2023-04-25 11:32:18 +0300

    fix: :bug: add axios

--- components/Project/BookList/BookCreate.js
+++ components/Project/BookList/BookCreate.js
@@ -2,6 +2,8 @@ import { useState } from 'react'
 
 import { useRouter } from 'next/router'
 
+import axios from 'axios'
+
 import { useTranslation } from 'next-i18next'
 
 import Modal from 'components/Modal'
