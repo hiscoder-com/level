ef967bf1224da02178e7cd3919cdf461975d2e05
valyukhov <alexzed@bk.ru>
2023-04-28 19:55:35 +0300

    feat: :sparkles: add new select

--- components/ProjectEdit/Participants/AssignPartiсipant.js
+++ components/ProjectEdit/Participants/AssignPartiсipant.js
@@ -1,5 +1,9 @@
 import { useTranslation } from 'next-i18next'
+import { Listbox } from '@headlessui/react'
+
 import Modal from 'components/Modal'
+
+import Down from 'public/arrow-down.svg'
 
 function AssignParticipant({
   openModalAssign,
@@ -8,6 +12,7 @@ function AssignParticipant({
   selectedUser,
   listOfAssigned,
   assign,
+  label,
   role,
 }) {
   const { t } = useTranslation()
@@ -18,33 +23,53 @@ function AssignParticipant({
         setOpenModalAssign(false)
         setSelectedUser('')
       }}
+      additionalClasses="overflow-y-visible"
     >
-      <div className="flex flex-col gap-7 min-h-[15vh]">
-        <select
-          className="input"
-          value={selectedUser}
-          onChange={(e) => setSelectedUser(e.target.value)}
-        >
-          {listOfAssigned?.map((el) => (
-            <option value={el.id} key={el.id}>
-              {el.login}
-            </option>
-          ))}
-        </select>
-        <div className="flex flex-row justify-center gap-7">
+      <div className="flex flex-col min-h-[15vh] gap-7">
+        <div className="text-2xl text-center">{t(label)}</div>
+        <Listbox value={selectedUser} onChange={(e) => setSelectedUser(e)}>
+          {({ open }) => (
+            <>
+              <div className="relative text-slate-900">
+                <Listbox.Button className="relative flex justify-between px-5 py-3 w-full bg-white rounded-lg">
+                  <span>
+                    {listOfAssigned?.length > 0 &&
+                      listOfAssigned?.find((el) => el.id === selectedUser)?.login}
+                  </span>
+                  <Down className="w-6 h-6 min-w-[1.5rem]" />
+                </Listbox.Button>
+                <div className={`-mt-2 pt-5 ${open ? 'bg-white' : ''}`}>
+                  <Listbox.Options className="absolute w-full max-h-[40vh] bg-white rounded-b-lg overflow-y-scroll ">
+                    {listOfAssigned.map((el) => (
+                      <Listbox.Option
+                        className="relative px-5 py-1 bg-white cursor-pointer hover:bg-teal-200 last:rounded-b-lg last:pb-3"
+                        key={el.id}
+                        value={el.id}
+                      >
+                        {el.login}
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </div>
+              </div>
+            </>
+          )}
+        </Listbox>
+
+        <div className="grid grid-cols-2 auto-cols-fr justify-center self-center w-1/2 gap-7">
           <button
             onClick={() => {
               assign(role)
               setSelectedUser(listOfAssigned?.[0]?.id)
             }}
             disabled={!selectedUser}
-            className="btn-link-full"
+            className="btn-link-full justify-center"
           >
             {t('Added')}
           </button>
 
           <button
-            className="btn-link-full"
+            className="btn-link-full justify-center"
             onClick={() => {
               setOpenModalAssign(false)
               setSelectedUser('')
