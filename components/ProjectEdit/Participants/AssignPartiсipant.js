af144f800a697bfa7a061e1d46fe6de9d0a30cdb
valyukhov <alexzed@bk.ru>
2023-05-04 15:04:09 +0300

    feat: :sparkles: add feature

--- components/ProjectEdit/Participants/AssignPartiсipant.js
+++ components/ProjectEdit/Participants/AssignPartiсipant.js
@@ -1,9 +1,8 @@
 import { useTranslation } from 'next-i18next'
-import { Listbox } from '@headlessui/react'
 
 import Modal from 'components/Modal'
 
-import Down from 'public/arrow-down.svg'
+import ListBox from 'components/ListBox'
 
 function AssignParticipant({
   openModalAssign,
@@ -27,34 +26,11 @@ function AssignParticipant({
     >
       <div className="flex flex-col min-h-[15vh] gap-7">
         <div className="text-2xl text-center">{t(label)}</div>
-        <Listbox value={selectedUser} onChange={(e) => setSelectedUser(e)}>
-          {({ open }) => (
-            <>
-              <div className="relative text-slate-900">
-                <Listbox.Button className="relative flex justify-between px-5 py-3 w-full bg-white rounded-lg">
-                  <span>
-                    {listOfAssigned?.length > 0 &&
-                      listOfAssigned?.find((el) => el.id === selectedUser)?.login}
-                  </span>
-                  <Down className="w-6 h-6 min-w-[1.5rem]" />
-                </Listbox.Button>
-                <div className={`-mt-2 pt-5 ${open ? 'bg-white' : ''}`}>
-                  <Listbox.Options className="absolute w-full max-h-[40vh] bg-white rounded-b-lg overflow-y-scroll ">
-                    {listOfAssigned.map((el) => (
-                      <Listbox.Option
-                        className="relative px-5 py-1 bg-white cursor-pointer hover:bg-teal-200 last:rounded-b-lg last:pb-3"
-                        key={el.id}
-                        value={el.id}
-                      >
-                        {el.login}
-                      </Listbox.Option>
-                    ))}
-                  </Listbox.Options>
-                </div>
-              </div>
-            </>
-          )}
-        </Listbox>
+        <ListBox
+          options={listOfAssigned.map((el) => ({ label: el.login, value: el.id }))}
+          selectedOption={selectedUser}
+          setSelectedOption={setSelectedUser}
+        />
 
         <div className="grid grid-cols-2 auto-cols-fr justify-center self-center w-1/2 gap-7">
           <button
