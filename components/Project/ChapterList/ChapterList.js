368894b2e35ded93facd8c7ddb76427a9032651a
valyukhov <alexzed@bk.ru>
2023-05-14 18:32:52 +0300

    refactor: :recycle: add num step in chapterList

--- components/Project/ChapterList/ChapterList.js
+++ components/Project/ChapterList/ChapterList.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 import { useTranslation } from 'next-i18next'
 
@@ -9,16 +10,25 @@ import Download from '../Download'
 import Modal from 'components/Modal'
 import Breadcrumbs from 'components/Breadcrumbs'
 
-import { useAccess, useGetChapters, useGetCreatedChapters, useProject } from 'utils/hooks'
+import {
+  useAccess,
+  useBriefState,
+  useGetChapters,
+  useGetCreatedChapters,
+  useProject,
+} from 'utils/hooks'
+import { supabase } from 'utils/supabaseClient'
+import { readableDate } from 'utils/helper'
+
+import { useCurrentUser } from 'lib/UserContext'
 
 import Plus from '/public/plus.svg'
-import { readableDate } from 'utils/helper'
-import { useCurrentUser } from 'lib/UserContext'
 
 function ChapterList() {
   const { user } = useCurrentUser()
 
   const {
+    locale,
     query: { code, bookid },
     push,
   } = useRouter()
@@ -31,7 +41,12 @@ function ChapterList() {
   const [isOpenDownloading, setIsOpenDownloading] = useState(false)
   const [creatingChapter, setCreatingChapter] = useState(false)
   const [downloadingChapter, setDownloadingChapter] = useState(null)
+  const [currentSteps, setCurrentSteps] = useState([])
   const [project] = useProject({ token: user?.access_token, code })
+  const { briefResume, isBrief } = useBriefState({
+    token: user?.access_token,
+    project_id: project?.id,
+  })
 
   const [chapters, { mutate: mutateChapters }] = useGetChapters({
     token: user?.access_token,
@@ -52,6 +67,41 @@ function ChapterList() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [chapters])
 
+  useEffect(() => {
+    if (project?.id) {
+      supabase
+        .rpc('get_current_steps', { project_id: project.id })
+        .then((res) => setCurrentSteps(res.data))
+    }
+  }, [project?.id])
+
+  const getCurrentStep = (chapter, index) => {
+    const step = currentSteps
+      ?.filter((step) => step.book === bookid)
+      ?.find((step) => step.chapter === chapter.num)
+    if (step) {
+      return (
+        <>
+          {!(!isBrief || briefResume) ? (
+            <Link href={`/projects/${project?.code}/edit/brief`}>
+              <a onClick={(e) => e.stopPropagation()} className="">
+                {t(isCoordinatorAccess ? 'EditBrief' : 'OpenBrief')}
+              </a>
+            </Link>
+          ) : (
+            <Link
+              key={index}
+              href={`/translate/${step.project}/${step.book}/${step.chapter}/${step.step}/intro`}
+            >
+              <a onClick={(e) => e.stopPropagation()} className="text-sm xl:text-xl">
+                {step.step} {t('Step').toLowerCase()}
+              </a>
+            </Link>
+          )}
+        </>
+      )
+    }
+  }
   return (
     <div className="pb-10">
       <div className="card mx-auto max-w-7xl">
@@ -71,8 +121,8 @@ function ChapterList() {
                   return (
                     <div
                       key={id}
-                      className={`${
-                        !isCreated && isCoordinatorAccess ? 'verse-block ' : ''
+                      className={`h-24 ${
+                        !isCreated && isCoordinatorAccess ? 'verse-block' : ''
                       } `}
                       onClick={() =>
                         isCreated &&
@@ -82,7 +132,7 @@ function ChapterList() {
                       }
                     >
                       <div
-                        className={`flex flex-col justify-between px-5 py-3 h-24 rounded-2xl cursor-pointer ${
+                        className={`flex flex-col justify-between px-5 py-3  rounded-2xl cursor-pointer h-24 ${
                           finished_at
                             ? 'bg-yellow-400'
                             : isCreated
@@ -90,20 +140,31 @@ function ChapterList() {
                             : 'bg-white border-2 border-slate-900'
                         }`}
                       >
-                        <div className="flex items-end gap-4">
+                        <div className="flex justify-between">
                           <div className="text-2xl font-bold">{num}</div>
-                          {started_at && (
-                            <div
-                              className={`text-sm ${
-                                !finished_at ? 'text-slate-400' : ''
-                              }`}
-                            >
-                              {readableDate(started_at)}
-                            </div>
-                          )}
+                          <div>
+                            {started_at && (
+                              <div
+                                className={`text-sm ${
+                                  !finished_at ? 'text-slate-400' : ''
+                                }`}
+                              >
+                                {readableDate(started_at, locale)}
+                              </div>
+                            )}
+                            {finished_at && (
+                              <div
+                                className={`text-sm ${
+                                  !finished_at ? 'text-slate-400' : ''
+                                }`}
+                              >
+                                {readableDate(finished_at, locale)}
+                              </div>
+                            )}
+                          </div>
                         </div>
                         <div className="text-sm"></div>
-                        {finished_at && (
+                        {finished_at ? (
                           <div
                             className="text-xl"
                             onClick={(e) => {
@@ -114,12 +175,14 @@ function ChapterList() {
                           >
                             <p className="text-sm xl:text-xl">{t('Download')}</p>
                           </div>
+                        ) : (
+                          getCurrentStep(chapter)
                         )}
                       </div>
                       <div
                         className={`${
                           isCreated ? 'hidden' : ''
-                        } flex justify-center items-center p-1 w-full h-full rounded-2xl border-0`}
+                        }  justify-center items-center p-1 w-full h-full rounded-2xl border-0`}
                         style={{
                           background: 'linear-gradient(90deg, #B7C9E5 1%, #A5B5CE 98%)',
                         }}
