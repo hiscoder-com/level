c7dbd3067b46c603630d82f83668468d43af8a5b
valyukhov <alexzed@bk.ru>
2023-07-06 16:26:09 +0300

    refactor: :recycle: refactor Steps

--- components/ProjectCreate/Steps.js
+++ components/ProjectCreate/Steps.js
@@ -5,78 +5,75 @@ import { Disclosure } from '@headlessui/react'
 import Down from 'public/arrow-down.svg'
 
 function Steps({ customSteps = [], updateStep, t }) {
+  const fields = [
+    { name: t('StepTitle'), type: 'title', textarea: false },
+    { name: t('StepDescription'), type: 'description', textarea: true },
+    { name: t('StepIntro'), type: 'intro', textarea: true },
+  ]
   return (
     <>
-      {customSteps?.map((el, index) => (
+      {customSteps?.map((step, index) => (
         <Disclosure key={index}>
-          <>
-            <Disclosure.Button className="flex justify-center gap-2 bg-gray-300 py-2 rounded-md">
-              <span>{el.title}</span>
-              <Down className="w-5 h-5" />
-            </Disclosure.Button>
-
-            <Disclosure.Panel className="flex-col gap-2">
-              <div className="flex items-center gap-2">
-                <span className="w-1/6">Название</span>
-                <StepTitle stepTitle={el.title} updateStep={updateStep} index={index} />
-              </div>
-              <div className="flex items-center gap-2 w-full">
-                <span className="w-1/6">Описание</span>
-                <Description
-                  stepDescription={el.description}
-                  updateStep={updateStep}
-                  index={index}
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex justify-between gap-2 bg-gray-300 py-2 px-4 rounded-md">
+                <span>{step.title}</span>
+                <Down
+                  className={`w-5 h-5 transition-transform duration-200 ${
+                    open ? 'rotate-180' : 'rotate-0'
+                  } `}
                 />
-              </div>
-              <div className="flex items-center gap-2 w-full">
-                <span className="w-1/6">Интро</span>
-                <Intro stepIntro={el.intro} updateStep={updateStep} index={index} />
-              </div>
-              <div className="flex items-center gap-2 w-full">
-                <span className="w-1/6">Инструменты</span>
-                <div className="flex flex-wrap items-center gap-2">
-                  {el.config[0].tools.map((item) => (
-                    <div
-                      key={item.name}
-                      className="btn-primary hover:bg-transparent hover:border-slate-600 p-2 rounded-md !cursor-auto"
-                    >
-                      {t('common:' + item.name)}
+              </Disclosure.Button>
+              <Disclosure.Panel className="space-y-7">
+                {fields.map((field) => (
+                  <div className="flex items-center gap-2 w-full" key={field.type}>
+                    <span className="w-1/6">{field.name}</span>
+                    <div className="w-5/6">
+                      <UpdateField
+                        value={step[field.type]}
+                        update={updateStep}
+                        index={index}
+                        type={field.type}
+                        textarea={field.textarea}
+                      />
                     </div>
-                  ))}
-                  |
-                  {el.config[1].tools.map((item) => (
-                    <div
-                      key={item.name}
-                      className="btn-primary hover:bg-transparent hover:border-slate-600 p-2 rounded-md !cursor-auto"
-                    >
-                      {t('common:' + item.name)}
-                    </div>
-                  ))}
-                  |
-                  {el.config[2]?.tools?.map((item) => (
-                    <div
-                      key={item.name}
-                      className="btn-primary hover:bg-transparent hover:border-slate-600 p-2 rounded-md !cursor-auto"
-                    >
-                      {t('common:' + item.name)}
-                    </div>
-                  ))}
+                  </div>
+                ))}
+                <div className="flex items-center w-full gap-2">
+                  <div className="w-1/6">{t('Tools')}</div>
+                  <div className="flex flex-wrap justify-start gap-2 w-5/6">
+                    {step.config.map((config, index) => (
+                      <div
+                        key={index}
+                        className="flex gap-2 pr-2 border-r border-slate-900 last:border-r-0"
+                      >
+                        {config.tools.map((tool) => (
+                          <div
+                            key={tool.name}
+                            className="btn-primary hover:bg-transparent hover:border-slate-600 p-2 rounded-md !cursor-auto"
+                          >
+                            {t('common:' + tool.name)}
+                          </div>
+                        ))}
+                      </div>
+                    ))}
+                  </div>
                 </div>
-              </div>
-              <div className="flex items-center gap-2 w-full">
-                <span className="w-1/6">Количество переводчиков</span>
-                <div className="btn-primary hover:bg-transparent hover:border-slate-600 p-2 rounded-md !cursor-auto">
-                  {el.count_of_users}
+                <div className="flex items-center gap-2 w-full">
+                  <span className="w-1/6">{t('TranslatorsCount')}</span>
+                  <div className="btn-primary hover:bg-transparent hover:border-slate-600 p-2 rounded-md !cursor-auto">
+                    {step.count_of_users}
+                  </div>
                 </div>
-              </div>
-              <div className="flex items-center gap-2 w-full">
-                <span className="w-1/6">Время выполнения</span>
-                <div className="btn-primary hover:bg-transparent hover:border-slate-600 p-2 rounded-md !cursor-auto">
-                  {el.time}
+                <div className="flex items-center gap-2 w-full">
+                  <span className="w-1/6">{t('ExecutionTime')}</span>
+                  <div className="btn-primary hover:bg-transparent hover:border-slate-600 p-2 rounded-md !cursor-auto">
+                    {step.time}
+                  </div>
                 </div>
-              </div>
-            </Disclosure.Panel>
-          </>
+              </Disclosure.Panel>
+            </>
+          )}
         </Disclosure>
       ))}
     </>
@@ -85,80 +82,22 @@ function Steps({ customSteps = [], updateStep, t }) {
 
 export default Steps
 
-// function UpdateField({ value, update, index,key }) {
-//   const [valueField, setValueField] = useState(value)
-//   useEffect(() => {
-//     if (value) {
-//       setValueField(value)
-//     }
-//   }, [value])
-
-//   return (
-//     <input
-//       className="input-primary"
-//       value={valueField}
-//       onChange={(e) => setValueField(e.target.value)}
-//       onBlur={() => {
-//         updateStep({ ref: { [key]: valueField.trim() }, index })
-//       }}
-//     />
-//   )
-// }
-
-function StepTitle({ stepTitle, updateStep, index }) {
-  const [title, setTitle] = useState(stepTitle)
+function UpdateField({ value, update, index, type, textarea = false, editable = true }) {
+  const [valueField, setValueField] = useState(value)
   useEffect(() => {
-    if (stepTitle) {
-      setTitle(stepTitle)
+    if (value) {
+      setValueField(value)
     }
-  }, [stepTitle])
-
-  return (
-    <input
-      className="input-primary"
-      value={title}
-      onChange={(e) => setTitle(e.target.value)}
-      onBlur={() => {
-        updateStep({ ref: { title: title.trim() }, index })
-      }}
-    />
-  )
-}
-
-function Description({ stepDescription, updateStep, index }) {
-  const [description, setDescription] = useState('')
-  useEffect(() => {
-    if (stepDescription) {
-      setDescription(stepDescription)
-    }
-  }, [stepDescription])
-  return (
-    <textarea
-      className="input-primary"
-      value={description}
-      onChange={(e) => setDescription(e.target.value)}
-      onBlur={() => {
-        updateStep({ ref: { description: description.trim() }, index })
-      }}
-    />
-  )
-}
-
-function Intro({ stepIntro, updateStep, index }) {
-  const [intro, setIntro] = useState('')
-  useEffect(() => {
-    if (stepIntro) {
-      setIntro(stepIntro)
-    }
-  }, [stepIntro])
-  return (
-    <textarea
-      className="input-primary"
-      value={intro}
-      onChange={(e) => setIntro(e.target.value)}
-      onBlur={() => {
-        updateStep({ ref: { intro: intro.trim() }, index })
-      }}
-    />
-  )
+  }, [value])
+  const props = {
+    className: 'input-primary',
+    value: valueField,
+    onChange: (e) => setValueField(e.target.value),
+    onBlur: () => {
+      update({ ref: { [type]: valueField.trim() }, index })
+    },
+    disabled: !editable,
+    rows: 6,
+  }
+  return <>{textarea ? <textarea {...props} /> : <input {...props} />}</>
 }
