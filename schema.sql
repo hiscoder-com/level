dacf90fbf3f01c72168e6051a04ba6b39fda4c44
valyukhov <alexzed@bk.ru>
2023-07-12 11:23:46 +0300

    feat: :sparkles: add update_project_basic in sql

--- schema.sql
+++ schema.sql
@@ -64,6 +64,7 @@
     DROP FUNCTION IF EXISTS PUBLIC.update_verses_in_chapters;
     DROP FUNCTION IF EXISTS PUBLIC.insert_additional_verses;
     DROP FUNCTION IF EXISTS PUBLIC.update_resources_in_projects;
+    DROP FUNCTION IF EXISTS PUBLIC.update_project_basic;
 
   -- END DROP FUNCTION
 
@@ -837,6 +838,31 @@
 
     END;
   $$;
+
+  -- create update_project_basic
+  CREATE FUNCTION PUBLIC.update_project_basic( project_code TEXT,title TEXT,orig_title TEXT,code TEXT, language_id BIGINT,user_id uuid ) RETURNS BOOLEAN
+    LANGUAGE plpgsql SECURITY DEFINER AS $$
+    DECLARE
+      project_id BIGINT;
+    BEGIN
+      IF project_code != update_project_basic.code THEN
+        SELECT id FROM public.projects WHERE projects.code = update_project_basic.code INTO project_id;
+        IF project_id IS NOT NULL THEN
+          RAISE EXCEPTION SQLSTATE '23505' USING MESSAGE = 'This project code is already in use';
+        END IF;
+      END IF;
+
+      SELECT id FROM public.projects WHERE projects.code = project_code INTO project_id;
+      IF authorize(auth.uid(), project_id) NOT IN ('admin') THEN
+        RAISE EXCEPTION SQLSTATE '42000' USING MESSAGE = 'No access rights to this function';
+      END IF;
+
+      UPDATE PUBLIC.projects SET code = update_project_basic.code,title=update_project_basic.title,orig_title=update_project_basic.orig_title,language_id=update_project_basic.language_id WHERE projects.id = project_id;
+
+      RETURN true;
+
+    END;
+   $$;
 -- END CREATE FUNCTION
 
 -- USERS
