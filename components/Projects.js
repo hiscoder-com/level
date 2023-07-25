79f45696ddcfce86d9688a388891816f56922b1d
Pavel <braurs64@gmail.com>
2023-07-25 15:08:03 +0400

    fix: :lipstick: fix project card position

--- components/Projects.js
+++ components/Projects.js
@@ -16,7 +16,7 @@ export default function Projects({ type }) {
     case 'projects':
       CurrentCard = ProjectCard
       className =
-        'grid grid-cols-1 gap-3 py-10 f-screen-account-tab overflow-y-auto md:grid-cols-2 xl:grid-cols-3 sm:gap-7 md:py-10'
+        'grid grid-cols-1 gap-3 py-10 content-start f-screen-account-tab overflow-y-auto md:grid-cols-2 xl:grid-cols-3 sm:gap-7 md:py-10'
       break
     case 'account':
       CurrentCard = ProjectPersonalCard
