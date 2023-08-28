322376771544f0c8c08408e11c971163bb5e5cb7
valyukhov <alexzed@bk.ru>
2023-08-28 15:15:29 +0300

    fix: :card_file_box: add rls migration and fix schema

--- migrations/20230719092318_add_rls_update_steps.sql
+++ migrations/20230719092318_add_rls_update_steps.sql
@@ -1,5 +1,5 @@
 DROP POLICY IF EXISTS "Изменять может админ" ON PUBLIC.steps;
 
-CREATE POLICY "Изменять может админ" ON PUBLIC.steps FOR
+CREATE POLICY "Обновлять может только админ" ON PUBLIC.steps FOR
   UPDATE
-    USING (authorize(auth.uid(), project_id) IN ('admin'));
+      USING (admin_only());
