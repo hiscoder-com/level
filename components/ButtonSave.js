be2d362c5385a856774ed2eb145c51deebcb5dde
valyukhov <alexzed@bk.ru>
2023-09-13 20:41:24 +0300

    fix: :bug: fix changing width of loading button

--- components/ButtonSave.js
+++ components/ButtonSave.js
@@ -2,8 +2,11 @@ import Loading from 'public/progress.svg'
 
 function ButtonSave({ children, onClick, isSaving }) {
   return (
-    <button className="btn-primary w-fit" onClick={onClick} disabled={isSaving}>
-      {isSaving ? <Loading className="w-6 animate-spin" /> : children}
+    <button className="relative btn-primary w-fit" onClick={onClick} disabled={isSaving}>
+      <span className={isSaving ? 'opacity-0' : 'opacity-100'}>{children}</span>
+      {isSaving && (
+        <Loading className="absolute mx-auto my-auto inset-0 w-6 animate-spin" />
+      )}
     </button>
   )
 }
