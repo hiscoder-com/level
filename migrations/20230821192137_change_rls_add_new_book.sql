b0afc789b6436775aa116f748b627141b09e31ae
valyukhov <alexzed@bk.ru>
2023-09-22 16:44:12 +0300

    chore: :wastebasket: clean migration files

--- migrations/20230821192137_change_rls_add_new_book.sql
+++
@@ -1,4 +0,0 @@
-DROP POLICY IF EXISTS "Добавлять можно только админу" ON PUBLIC.books;
-CREATE POLICY "Добавлять можно админу и координатору" ON PUBLIC.books FOR
-    INSERT
-      WITH CHECK (authorize(auth.uid(), project_id) IN ('admin', 'coordinator'));
---
+++ migrations/20230920101350_add_function_get_max_step.sql
@@ -0,0 +1,43 @@
+DROP FUNCTION IF EXISTS PUBLIC.get_max_step;
+CREATE OR REPLACE FUNCTION PUBLIC.get_max_step(
+  project_code TEXT, 
+  translator_id BIGINT, 
+  chapter_id BIGINT
+) RETURNS JSON
+LANGUAGE plpgsql SECURITY DEFINER AS $$   
+DECLARE
+  project_id BIGINT;
+  max_step INT2;
+  count_steps INT2;      
+BEGIN
+  SELECT id INTO project_id FROM projects WHERE code = project_code;
+
+  IF NOT authorize(auth.uid(), project_id) IN ('admin', 'supporter') THEN
+    RETURN json_build_object('count_steps', 0, 'max_step', 0);
+  END IF;
+
+  SELECT MAX(steps.sorting) INTO count_steps
+  FROM PUBLIC.steps 
+  WHERE steps.project_id = project_id;
+
+  SELECT MAX(steps.sorting) INTO max_step
+  FROM Public.progress 
+  LEFT JOIN PUBLIC.steps ON (steps.id = progress.step_id) 
+  WHERE progress.verse_id = (
+    SELECT verses.id 
+    FROM PUBLIC.verses 
+    WHERE verses.chapter_id = chapter_id 
+      AND project_translator_id = translator_id 
+    LIMIT 1
+  );
+
+  RETURN CASE
+    WHEN max_step IS NULL THEN
+      json_build_object('count_steps', count_steps, 'max_step', 1)
+    WHEN max_step = count_steps THEN
+      json_build_object('count_steps', count_steps, 'max_step', count_steps)
+    ELSE
+      json_build_object('count_steps', count_steps, 'max_step', max_step + 1)
+  END;
+END;  
+$$
