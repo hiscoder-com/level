0ccc72c3d2c4d0edf02318009725e264bb6e27da
Denis Arger <den.arger@gmail.com>
2023-10-27 16:38:06 +0300

    feat: add api ChaptersTranslateHandler

--- pages/api/projects/[code]/books/chapters_translate.js
+++ pages/api/projects/[code]/books/chapters_translate.js
@@ -1,7 +1,7 @@
 import { supabaseService } from 'utils/supabaseService'
 import supabaseApi from 'utils/supabaseServer'
 
-export default async function BooksIsNullLevelCheckHandler(req, res) {
+export default async function ChaptersTranslateHandler(req, res) {
   let supabase
   try {
     supabase = await supabaseApi({ req, res })
@@ -12,17 +12,29 @@ export default async function BooksIsNullLevelCheckHandler(req, res) {
     query: { code },
     method,
   } = req
+
   switch (method) {
     case 'GET':
       try {
-        const { data, error } = await supabaseService.rpc(
+        const { data: booksData, error: booksError } = await supabaseService.rpc(
           'get_books_not_null_level_checks',
           {
             project_code: code,
           }
         )
-        if (error) throw error
-        return res.status(200).json(data)
+        const { data: chaptersData, error: chaptersError } = await supabaseService.rpc(
+          'find_books_with_chapters_and_verses',
+          {
+            project_code: code,
+          }
+        )
+        if (chaptersError || booksError) {
+          return res.status(404).json({ booksError, chaptersError })
+        }
+
+        const combinedData = [...booksData, ...chaptersData]
+
+        return res.status(200).json(combinedData)
       } catch (error) {
         return res.status(404).json({ error })
       }
