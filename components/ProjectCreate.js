d6439b642c7cb475d08520b59d8a609601a4c833
valyukhov <alexzed@bk.ru>
2023-11-07 15:17:11 +0300

    fix: :bug: fix mobile background style

--- components/ProjectCreate.js
+++ components/ProjectCreate.js
@@ -162,7 +162,7 @@ function ProjectCreate() {
     <>
       <div className="py-0 sm:py-10" onClick={(e) => e.stopPropagation()}>
         <form className="flex flex-col gap-0 md:gap-4" onSubmit={handleSubmit(onSubmit)}>
-          <div className="card-md bg-th-background-tertiary py-7 space-y-7">
+          <div className="card-md sm:bg-th-background-tertiary py-7 space-y-7">
             <h3 className="text-xl font-bold">{t('project-edit:BasicInformation')}</h3>
             <BasicInformation
               t={t}
@@ -175,13 +175,13 @@ function ProjectCreate() {
               uniqueCheck
             />
           </div>
-          <div className="card-md bg-th-background-tertiary flex flex-col gap-7 py-7">
+          <div className="card-md sm:bg-th-background-tertiary flex flex-col gap-7 py-7">
             <h3 className="text-xl font-bold">{t('project-edit:Steps')}</h3>
             <div className="flex flex-col gap-7 text-sm md:text-base">
               <Steps customSteps={customSteps} updateSteps={updateSteps} />
             </div>
           </div>
-          <div className="card-md bg-th-background-tertiary flex flex-col gap-7 py-7">
+          <div className="card-md sm:bg-th-background-tertiary flex flex-col gap-7 py-7">
             <div className="flex justify-between">
               <h3 className="text-xl font-bold">{t('Brief')}</h3>
               <div className="flex items-center gap-2">
@@ -210,7 +210,7 @@ function ProjectCreate() {
               autoSave
             />
           </div>
-          <div className="card-md bg-th-background-tertiary flex flex-col gap-7 pb-7 mb-7 border-b border-th-border-primary">
+          <div className="card-md sm:bg-th-background-tertiary flex flex-col gap-7 pb-7 mb-7 border-b border-th-border-primary">
             <h3 className="text-xl font-bold">{t('common:ListResources')}</h3>
             <CommitsList
               methodId={methodId}
