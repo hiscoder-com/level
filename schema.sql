95ff19ed52c61672ee89edbc49e8870869f9ef94
Denis Arger <den.arger@gmail.com>
2023-11-13 10:53:23 +0300

    feat: add validation get_books_not_null_level_checks and  find_books_with_chapters_and_verses

--- schema.sql
+++ schema.sql
@@ -146,30 +146,51 @@
 -- getting all the books that specify the levels of checks
     CREATE OR REPLACE FUNCTION get_books_not_null_level_checks(project_code text)
     RETURNS TABLE (book_code public.book_code, level_checks json) AS $$
-      BEGIN
-        RETURN QUERY
-      SELECT
-        b.code AS book_code,
-        b.level_checks
+      DECLARE
+        project_id bigint;
+    BEGIN
+        SELECT id INTO project_id FROM public.projects WHERE code = project_code;
+
+      IF project_id IS NULL THEN
+        RETURN;
+      END IF;
+
+      IF authorize(auth.uid(), project_id) NOT IN ('user', 'admin', 'coordinator', 'moderator') THEN
+        RETURN;
+      END IF;
+
+      RETURN QUERY
+        SELECT
+            b.code AS book_code,
+            b.level_checks
         FROM
-        public.books b
-      INNER JOIN
-        public.projects p ON b.project_id = p.id
-      WHERE
-        p.code = project_code
-        AND b.level_checks IS NOT NULL;
+            public.books b
+        INNER JOIN
+            public.projects p ON b.project_id = p.id
+        WHERE
+            p.code = project_code
+            AND b.level_checks IS NOT NULL;
       END;
     $$ LANGUAGE plpgsql;
 
--- getting all books with verses that are started and non-zero translation texts
-    CREATE OR REPLACE FUNCTION find_books_with_chapters_and_verses(project_code text) 
-    RETURNS TABLE (book_code public.book_code, chapter_num smallint, verse_num smallint, verse_text text) AS $$
-    BEGIN
-    IF authorize(auth.uid(), get_project_id_by_code(project_code)) NOT IN ('user', 'admin', 'coordinator', 'moderator') THEN
-        RETURN;
-    END IF;
 
-    RETURN QUERY
+-- getting all books with verses that are started and non-zero translation texts
+    CREATE OR REPLACE FUNCTION find_books_with_chapters_and_verses(project_code text)
+    RETURNS TABLE (book_code public.book_code, chapter_num smallint, verse_num smallint, verse_text text) AS $$
+    DECLARE
+        project_id bigint;
+      BEGIN
+      SELECT id INTO project_id FROM public.projects WHERE code = project_code;
+
+      IF project_id IS NULL THEN
+        RETURN;
+      END IF;
+
+      IF authorize(auth.uid(), project_id) NOT IN ('user', 'admin', 'coordinator', 'moderator') THEN
+        RETURN;
+      END IF;
+
+      RETURN QUERY
         SELECT
             b.code AS book_code,
             c.num AS chapter_num,
@@ -189,6 +210,7 @@
             AND p.code = project_code;
         END;
       $$ LANGUAGE plpgsql;
+
 
 
   -- conditions for the user to have access to the site: 2 checkboxes and the user was not blocked
