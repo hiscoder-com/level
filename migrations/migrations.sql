fbba422ba301f2d15452fb435a4f06efc08ce5f5
Denis Arger <den.arger@gmail.com>
2023-11-13 11:18:16 +0300

    feat: add  get_books_not_null_level_checks and   find_books_with_chapters_and_verses in migration

--- migrations/migrations.sql
+++ migrations/migrations.sql
@@ -1999,3 +1999,72 @@
     WHERE steps.project_id = projects.id AND projects.type = 'obs' AND steps.sorting = '7';
 
     ALTER TABLE public.projects ALTER COLUMN dictionaries_alphabet SET DEFAULT '[]';
+
+
+-- getting all the books that specify the levels of checks
+    CREATE OR REPLACE FUNCTION get_books_not_null_level_checks(project_code text)
+    RETURNS TABLE (book_code public.book_code, level_checks json) AS $$
+      DECLARE
+        project_id bigint;
+    BEGIN
+        SELECT id INTO project_id FROM public.projects WHERE code = project_code;
+
+      IF project_id IS NULL THEN
+        RETURN;
+      END IF;
+
+      IF authorize(auth.uid(), project_id) NOT IN ('user', 'admin', 'coordinator', 'moderator') THEN
+        RETURN;
+      END IF;
+
+      RETURN QUERY
+        SELECT
+            b.code AS book_code,
+            b.level_checks
+        FROM
+            public.books b
+        INNER JOIN
+            public.projects p ON b.project_id = p.id
+        WHERE
+            p.code = project_code
+            AND b.level_checks IS NOT NULL;
+      END;
+    $$ LANGUAGE plpgsql;
+
+
+-- getting all books with verses that are started and non-zero translation texts
+    CREATE OR REPLACE FUNCTION find_books_with_chapters_and_verses(project_code text)
+    RETURNS TABLE (book_code public.book_code, chapter_num smallint, verse_num smallint, verse_text text) AS $$
+    DECLARE
+        project_id bigint;
+      BEGIN
+      SELECT id INTO project_id FROM public.projects WHERE code = project_code;
+
+      IF project_id IS NULL THEN
+        RETURN;
+      END IF;
+
+      IF authorize(auth.uid(), project_id) NOT IN ('user', 'admin', 'coordinator', 'moderator') THEN
+        RETURN;
+      END IF;
+
+      RETURN QUERY
+        SELECT
+            b.code AS book_code,
+            c.num AS chapter_num,
+            v.num AS verse_num,
+            v.text AS verse_text
+        FROM
+            public.books b
+        INNER JOIN
+            public.chapters c ON b.id = c.book_id
+        INNER JOIN
+            public.verses v ON c.id = v.chapter_id
+        INNER JOIN
+            public.projects p ON b.project_id = p.id
+        WHERE
+            c.started_at IS NOT NULL
+            AND v.text IS NOT NULL
+            AND p.code = project_code;
+        END;
+      $$ LANGUAGE plpgsql;
