842533ddb19f1a2a8a0ce997a76b18be6285966b
valyukhov <alexzed@bk.ru>
2023-11-21 19:26:19 +0300

    fix: :pencil2: rename variable

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require('./next-i18next.config')
-const isIntranet = process.env.INTRANET ? { output: 'standalone' } : {}
+const intranetOption = process.env.INTRANET ? { output: 'standalone' } : {}
 const nextConfig = {
   reactStrictMode: true,
-  ...isIntranet,
+  ...intranetOption,
   transpilePackages: ['swagger-ui-react', 'swagger-client', 'react-syntax-highlighter'],
   eslint: {
     dirs: ['pages', 'utils', 'lib', 'components'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
