214630c00c73c3f22f08f7b9f0ef13df0813fc9a
valyukhov <alexzed@bk.ru>
2024-06-08 15:33:45 +0300

    fix: :bug: add local fonts for Intranet

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import { appWithTranslation } from 'next-i18next'
-import localFont from 'next/font/local'
 import { RecoilRoot } from 'recoil'
 
 import Layout from 'components/Layout'
@@ -11,16 +10,17 @@ import 'styles/globals.css'
 import useSupabaseClient from 'utils/supabaseClient'
 import { useGetTheme } from 'utils/hooks'
 
-const roboto = localFont({ src: './RobotoFlex.ttf', variable: '--font-roboto' })
+import { roboto } from 'public/fonts/fonts'
 
 function MyApp({ Component, pageProps }) {
+  const isIntranet = process.env.NEXT_PUBLIC_INTRANET ?? false
   const supabaseClient = useSupabaseClient()
   useGetTheme()
   if (Component.layoutType == 'empty') {
     return (
       <UserContextProvider supabaseClient={supabaseClient}>
         <RecoilRoot>
-          <main className={`${roboto.variable} font-sans`}>
+          <main className={isIntranet ? roboto.className : ''}>
             <Component {...pageProps} />
           </main>
         </RecoilRoot>
@@ -31,7 +31,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <UserContextProvider supabaseClient={supabaseClient}>
       <RecoilRoot>
-        <main className={`${roboto.variable} font-sans`}>
+        <main className={isIntranet ? roboto.className : ''}>
           <Layout backgroundColor={Component.backgroundColor ?? 'bg-th-secondary-100'}>
             <Component {...pageProps} />
           </Layout>
