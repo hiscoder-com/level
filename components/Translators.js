47af01c9716fdf5419f930e9cd8437f60e2b3d54
Pavel <braurs64@gmail.com>
2024-07-02 14:44:49 +0400

    feat: added translate

--- components/Translators.js
+++ components/Translators.js
@@ -1,19 +1,22 @@
-import TranslatorImage from 'components/TranslatorImage'
 import { useMemo, useState } from 'react'
 
+import { useTranslation } from 'next-i18next'
+
 import Modal from './Modal'
+import TranslatorImage from 'components/TranslatorImage'
 
 import { useTranslators } from 'utils/hooks'
 
 function Translators({
   projectCode,
   size,
-  clickable = false,
   className,
   showModerator,
   isStep = false,
+  clickable = false,
   activeTranslators = [],
 }) {
+  const { t } = useTranslation('common')
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [projectTranslators] = useTranslators({
     code: projectCode,
@@ -61,34 +64,37 @@ function Translators({
       <Modal
         isOpen={isModalOpen}
         closeHandle={() => setIsModalOpen(false)}
-        title="All Translators"
+        title={t('AllTranslators')}
         className={{
           dialogTitle: 'text-2xl font-medium leading-6',
           dialogPanel:
             'w-full max-w-md px-7 py-10 align-middle transform overflow-y-auto shadow-xl transition-all bg-th-primary-100 text-th-text-secondary-100 rounded-3xl',
         }}
       >
-        <div className="flex flex-wrap gap-1.5 my-6 py-3 max-h-[50vh] overflow-y-auto">
+        <div className="flex flex-wrap gap-x-1.5 gap-y-2.5 my-6 py-3 max-h-[50vh] overflow-y-auto">
           {translators.map((participant, index) => (
-            <div key={index} className="flex items-center pb-1">
-              <div className="flex flex-1 items-center gap-2.5 pl-1 pr-3 py-1 border rounded-3xl w-5/6 text-th-text-secondary-100">
-                <div className="w-8 h-8 min-w-[2rem]">
-                  <TranslatorImage
-                    item={participant}
-                    clickable={clickable}
-                    showModerator={showModerator}
-                  />
-                </div>
-                <div>
-                  <p className="text-lg">{participant.users.login}</p>
-                </div>
+            <div
+              key={index}
+              className="group flex items-center gap-2.5 pl-1 pr-3 py-1 border rounded-3xl"
+            >
+              <div
+                className={`w-8 h-8 ${
+                  clickable ? 'transition-opacity	duration-300 group-hover:opacity-50' : ''
+                }`}
+              >
+                <TranslatorImage
+                  item={participant}
+                  clickable={clickable}
+                  showModerator={showModerator}
+                />
               </div>
+              <p className="text-lg cursor-default">{participant.users.login}</p>
             </div>
           ))}
         </div>
         <div className="text-center">
           <button className="btn-secondary" onClick={() => setIsModalOpen(false)}>
-            Close
+            {t('Close')}
           </button>
         </div>
       </Modal>
