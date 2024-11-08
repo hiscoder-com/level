4e418f0faa1e59122a6752c025834b50178f5679
Pavel <braurs64@gmail.com>
2024-11-08 08:20:45 +0400

    feat: upd Partners

--- components/StartPage/Partners.js
+++ components/StartPage/Partners.js
@@ -1,22 +1,58 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
+const partners = [
+  {
+    name: 'Unfolding Word',
+    url: 'https://www.unfoldingword.org/',
+    logo: '/icons/unfolding-word.svg',
+    width: 201,
+    height: 35,
+  },
+  {
+    name: 'Aquifer',
+    url: 'https://aquifer.bible/',
+    logo: '/icons/aquifer.svg',
+    width: 102,
+    height: 37,
+  },
+  {
+    name: 'Glokas',
+    url: 'https://glokas.com/',
+    logo: '/icons/glokas.svg',
+    width: 126,
+    height: 23,
+  },
+  {
+    name: 'BibleVis',
+    url: 'https://biblevis.com/',
+    logo: '/icons/biblevis.svg',
+    width: 141,
+    height: 22,
+  },
+]
+
 function Partners({ t }) {
   return (
     <div className="flex w-full flex-col gap-6 md:gap-14">
       <p className="font-semibold md:font-bold">{t('Partners')}</p>
-      <div
-        className="flex h-32 w-full items-center justify-center rounded-xl bg-th-secondary-100 md:h-56"
-        onClick={(e) => e.stopPropagation()}
-      >
-        <Link href="https://www.unfoldingword.org/" target="_blank">
-          <Image
-            src="/icons/unfolding-word.svg"
-            alt="Unfolding Word"
-            width={201}
-            height={35}
-          />
-        </Link>
+      <div className="grid grid-cols-2 gap-3">
+        {partners.map((partner) => (
+          <div
+            key={partner.name}
+            className="flex h-28 w-full items-center justify-center rounded-xl bg-th-secondary-100 px-8 py-11"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <Link href={partner.url} target="_blank">
+              <Image
+                src={partner.logo}
+                alt={partner.name}
+                width={partner.width}
+                height={partner.height}
+              />
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   )
