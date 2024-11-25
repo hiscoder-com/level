785595b0f4b76f9e180a78e031977f590825ab9f
valyukhov <alexzed@bk.ru>
2024-11-25 08:46:56 +0300

    fix: :bug: fix routing in main page

--- components/StartPage/SectionBlock.js
+++ components/StartPage/SectionBlock.js
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 import Close from 'public/icons/close.svg'
@@ -41,14 +40,6 @@ function SectionBlock({
           showSection ? '' : 'hidden'
         }`}
       />
-      {showSection && (
-        <Link
-          href="/"
-          className="absolute z-10 cursor-pointer"
-          onClick={(e) => e.stopPropagation()}
-          scroll={false}
-        ></Link>
-      )}
     </div>
   )
 }
