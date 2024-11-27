49d86dec399742764c605005d58988680ab01cdd
DenisArger <den.arger@gmail.com>
2024-11-27 14:06:59 +0300

    fix: show Back for root item

--- components/Panel/UI/TAContent.js
+++ components/Panel/UI/TAContent.js
@@ -19,19 +19,22 @@ function TAContent({ item, setHref, config, goBack }) {
     >
       <div className="flex h-full flex-col">
         <div className="sticky top-0 z-10 flex items-center bg-th-secondary-10 pb-4">
-          <div
-            className="mr-2.5 h-fit w-fit cursor-pointer rounded-full bg-th-secondary-100 p-1 hover:opacity-70"
-            onClick={handleBackClick}
-          >
-            <Back className="w-8 stroke-th-primary-200" />
-          </div>
-          {!['intro', 'front'].includes(item?.title) && (
-            <div className="mt-1 text-xl font-bold">
-              <ReactMarkdown className="bg-th-secondary-10 text-2xl">
-                {item?.title}
-              </ReactMarkdown>
+          {![
+            'Что такое «Академия перевода»?',
+            'What is unfoldingWord® Translation Academy?',
+          ].includes(item?.title) && (
+            <div
+              className="mr-2.5 h-fit w-fit cursor-pointer rounded-full bg-th-secondary-100 p-1 hover:opacity-70"
+              onClick={handleBackClick}
+            >
+              <Back className="w-8 stroke-th-primary-200" />
             </div>
           )}
+          <div className="mt-1 text-xl font-bold">
+            <ReactMarkdown className="bg-th-secondary-10 text-2xl">
+              {item?.title}
+            </ReactMarkdown>
+          </div>
         </div>
 
         <div className="flex-1 pb-4">
